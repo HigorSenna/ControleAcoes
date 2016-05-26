aaad7cecf28e250c7d6253e8cf6ad8bb7804f66a
Higor Senna <higorr_jf@hotmail.com>
2016-05-26 01:24:47 -0300

    Ajustes tela, criado açao de exibir msg sucesso

--- src/main/java/br/com/javaweb/controller/CadastroInvestidorContaController.java
+++ src/main/java/br/com/javaweb/controller/CadastroInvestidorContaController.java
@@ -1,11 +1,14 @@
 package br.com.javaweb.controller;
 
+import java.io.IOException;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.view.ViewScoped;
 
 import br.com.javaweb.model.ContaBancaria;
 import br.com.javaweb.model.Investidor;
 import br.com.javaweb.service.InvestidorService;
+import br.com.javaweb.utils.MessagesAndRedirect;
 
 @ManagedBean
 @ViewScoped
@@ -13,16 +16,20 @@ public class CadastroInvestidorContaController {
 	private ContaBancaria contaBancaria = new ContaBancaria();
 	private Investidor investidor = new Investidor();
 	private InvestidorService investidorService = new InvestidorService();
-	
-	public void salvar(){
-		try{
+
+	public void salvar() {
+		try {
 			investidor.setIdConta(contaBancaria);
-			investidorService.salvar(investidor);	
+			investidorService.salvar(investidor);
+			MessagesAndRedirect.exibirMensagemSucessoRedirect("Registro inserido com sucesso!", "principal.xhtml");
+		} catch (Exception ex) {
+			ex.getMessage();//erro ao salvar ou ao redirecionar
+			try {
+				MessagesAndRedirect.exibirMensagemErroRedirect("Registro não Inserido!", "principal.xhtml");
+			} catch (Exception e) {
+				e.getMessage();//erro ao tentar redirecionar 
+			}
 		}
-		catch(Exception ex){			
-			ex.getMessage();
-		}
-			
 	}
 
 	public ContaBancaria getContaBancaria() {
@@ -47,8 +54,6 @@ public class CadastroInvestidorContaController {
 
 	public void setInvestidorService(InvestidorService investidorService) {
 		this.investidorService = investidorService;
-	}	
-	
-	
-	
+	}
+
 }

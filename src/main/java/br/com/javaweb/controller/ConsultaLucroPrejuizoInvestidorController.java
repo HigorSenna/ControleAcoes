f9ef6f4e18cc8809e81a1d0bdece247d48d75ad3
Higor Senna <higorr_jf@hotmail.com>
2016-06-12 12:54:12 -0300

    Lucros e Prejuizos buscando certo

--- src/main/java/br/com/javaweb/controller/ConsultaLucroPrejuizoInvestidorController.java
+++ src/main/java/br/com/javaweb/controller/ConsultaLucroPrejuizoInvestidorController.java
@@ -1,20 +1,23 @@
 package br.com.javaweb.controller;
 
-import java.util.List;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 import br.com.javaweb.model.Investidor;
-import br.com.javaweb.service.InvestidorService;
+import br.com.javaweb.utils.Session;
 
 @ManagedBean
 @ViewScoped
 public class ConsultaLucroPrejuizoInvestidorController {
-	private InvestidorService investidorService;
 	
-	public List<Investidor> buscarInvestidores(){
-		investidorService = new InvestidorService();
-		return investidorService.buscarInvestidores();
+	private Investidor investidor;
+	
+	public Investidor buscarInvestidor(){
+		investidor = buscarInvestidorSessao();
+		return investidor;
 	}
+	
+	private Investidor buscarInvestidorSessao(){
+		return (Investidor) Session.pegarSessao();		
+	}	
 }

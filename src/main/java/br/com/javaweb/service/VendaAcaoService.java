58d33168957f779cb7f54af98b66e720c8285e5c
Higor Senna Chaves <hschaves@stefanini.com>
2016-06-14 11:24:56 -0300

    Retirado items desnecessários

--- src/main/java/br/com/javaweb/service/VendaAcaoService.java
+++ src/main/java/br/com/javaweb/service/VendaAcaoService.java
@@ -6,16 +6,12 @@ import java.util.List;
 import br.com.javaweb.model.Acao;
 import br.com.javaweb.model.Investidor;
 import br.com.javaweb.transacoes.model.HistoricoTransacao;
-import br.com.javaweb.transacoes.model.Venda;
 
 public class VendaAcaoService {
-	private List<Venda> vendas;
 	private List<HistoricoTransacao> historico;
 	
 	public List<HistoricoTransacao> verificarValorVendaCadaAcaoInvestidor(Investidor investidor,List<Acao> acoes){
 		historico = new ArrayList<>();
-		vendas = new ArrayList<>();
-		Venda venda = new Venda();
 		
 		historico = investidor.getHistoricosTransacoesList();		
 		

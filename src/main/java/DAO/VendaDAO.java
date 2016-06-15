58466219c588e83ad1c273795327a1b414e29dec
hschaves <hschaves@STFJFAC017253-W.stefanini.dom>
2016-06-15 11:59:03 -0300

    Inicializado Regras de negócio em VendaAcaoService, criado metodos em VendaDAO

--- src/main/java/DAO/VendaDAO.java
+++ src/main/java/DAO/VendaDAO.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
+import br.com.javaweb.model.ContaBancaria;
+import br.com.javaweb.transacoes.model.HistoricoTransacao;
 import br.com.javaweb.transacoes.model.Venda;
 
 public class VendaDAO implements Serializable {
@@ -18,6 +20,32 @@ public class VendaDAO implements Serializable {
 
     public EntityManager getEntityManager() {
         return emf.createEntityManager();
+    }
+    
+    public void atualizarHistoricoInvestidor(HistoricoTransacao historico){
+    	 EntityManager em = null;
+    	 em = getEntityManager();
+    	 
+         em.getTransaction().begin();
+         	em.merge(historico);
+         em.getTransaction().commit();
+         
+         if (em != null) {
+             em.close();
+         }
+    }
+    
+    public void atualizarContaBancaria(ContaBancaria conta){
+    	 EntityManager em = null;
+    	 em = getEntityManager();
+    	 
+         em.getTransaction().begin();
+         	em.merge(conta);
+         em.getTransaction().commit();
+         
+         if (em != null) {
+             em.close();
+         }
     }
    
     public void inserirVenda(Venda venda){
--- src/main/java/br/com/javaweb/service/VendaAcaoService.java
+++ src/main/java/br/com/javaweb/service/VendaAcaoService.java
@@ -3,12 +3,57 @@ package br.com.javaweb.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import DAO.VendaDAO;
 import br.com.javaweb.model.Acao;
+import br.com.javaweb.model.ContaBancaria;
 import br.com.javaweb.model.Investidor;
 import br.com.javaweb.transacoes.model.HistoricoTransacao;
+import br.com.javaweb.transacoes.model.Venda;
 
 public class VendaAcaoService {
+	
+	private EntityManagerFactory emf = Persistence.createEntityManagerFactory("ControleAcoes");
+	
+	private VendaDAO vendaDAO;
+	
 	private List<HistoricoTransacao> historico;
+	
+	public void inserirVenda(Venda venda,int quantidadeVenda){
+		vendaDAO.inserirVenda(venda);	
+		atualizarContaInvestidor(venda.getIdInvestidor().getIdConta(),venda.getValorRecebido());
+		HistoricoTransacao historicoAtualizar = buscarRegistroHistoricoParaAtualizar(venda.getIdInvestidor().getHistoricosTransacoesList(), venda.getNomeAcao());
+		atualizarHistoricoInvestidor(historicoAtualizar, quantidadeVenda);		
+	}
+	
+	private void atualizarHistoricoInvestidor(HistoricoTransacao historico,int quantidadeVenda){
+		int quantidadeExistente = historico.getQuantidadeTotal();
+		int quantidadeAtualizada = quantidadeExistente - quantidadeVenda;
+		historico.setQuantidadeTotal(quantidadeAtualizada);
+		
+		vendaDAO.atualizarHistoricoInvestidor(historico);
+	}
+	
+	private HistoricoTransacao buscarRegistroHistoricoParaAtualizar(List<HistoricoTransacao> historicos, String nomeAcao){
+		HistoricoTransacao historicoAtualizar = null;
+		for(HistoricoTransacao historico: historicos){
+			if(historico.getNomeAcao().equals(nomeAcao)){
+				historicoAtualizar = historico;
+			}
+		}
+		return historicoAtualizar;
+	}
+	
+	private void atualizarContaInvestidor(ContaBancaria conta,double valorDaVenda){
+		vendaDAO = new VendaDAO(emf);
+		double saldoExistente = conta.getSaldo();
+		double saldoAtualizado = saldoExistente + valorDaVenda;
+		conta.setSaldo(saldoAtualizado);	
+		
+		vendaDAO.atualizarContaBancaria(conta);
+	}
 	
 	public List<HistoricoTransacao> verificarValorVendaCadaAcaoInvestidor(Investidor investidor,List<Acao> acoes){
 		historico = new ArrayList<>();

ae432868f37cc34730b0f7f2b8003a5aa12c22c6
Higor Senna <higorr_jf@hotmail.com>
2016-06-16 00:32:57 -0300

    Tela lucro/Prejuízo inicializada

--- src/main/java/br/com/javaweb/controller/ConsultaLucroPrejuizoInvestidorController.java
+++ src/main/java/br/com/javaweb/controller/ConsultaLucroPrejuizoInvestidorController.java
@@ -1,9 +1,12 @@
 package br.com.javaweb.controller;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 import br.com.javaweb.model.Investidor;
+import br.com.javaweb.transacoes.model.Venda;
 import br.com.javaweb.utils.Session;
 
 @ManagedBean
@@ -12,9 +15,9 @@ public class ConsultaLucroPrejuizoInvestidorController {
 	
 	private Investidor investidor;
 	
-	public Investidor buscarInvestidor(){
+	public List<Venda> buscarVendasInvestidor(){
 		investidor = buscarInvestidorSessao();
-		return investidor;
+		return investidor.getVendasList();
 	}
 	
 	private Investidor buscarInvestidorSessao(){

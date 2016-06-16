b0d7120615b8f822c3acb15e6888f1958d633427
Higor Senna Chaves <hschaves@stefanini.com>
2016-06-16 10:38:08 -0300

    Lucros e prejuizos atualizando em tempo real funcionando

--- src/main/java/br/com/javaweb/controller/ConsultaLucroPrejuizoInvestidorController.java
+++ src/main/java/br/com/javaweb/controller/ConsultaLucroPrejuizoInvestidorController.java
@@ -2,10 +2,12 @@ package br.com.javaweb.controller;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 import br.com.javaweb.model.Investidor;
+import br.com.javaweb.service.InvestidorService;
 import br.com.javaweb.transacoes.model.Venda;
 import br.com.javaweb.utils.Session;
 
@@ -14,9 +16,20 @@ import br.com.javaweb.utils.Session;
 public class ConsultaLucroPrejuizoInvestidorController {
 	
 	private Investidor investidor;
+	private InvestidorService investidorService;
+	
+	@PostConstruct
+	public void init(){
+		investidor = buscarInvestidor();
+	}
+	
+	private Investidor buscarInvestidor(){
+		investidorService = new InvestidorService();
+		investidor = buscarInvestidorSessao();
+		return investidorService.buscarInvestidorLoginSenha(investidor.getLogin(), investidor.getSenha());
+	}
 	
 	public List<Venda> buscarVendasInvestidor(){
-		investidor = buscarInvestidorSessao();
 		return investidor.getVendasList();
 	}
 	

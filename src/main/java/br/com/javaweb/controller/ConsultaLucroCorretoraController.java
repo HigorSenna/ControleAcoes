79f55eaa6508a86cdc3d89b2072884b6249748a8
Higor Senna <higorr_jf@hotmail.com>
2016-07-02 20:58:15 -0300

    Permissão de acesso a tela de Consultar lucro da corretora feita.

--- src/main/java/br/com/javaweb/controller/ConsultaLucroCorretoraController.java
+++ src/main/java/br/com/javaweb/controller/ConsultaLucroCorretoraController.java
@@ -7,8 +7,11 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 import br.com.javaweb.model.Corretora;
+import br.com.javaweb.model.Investidor;
 import br.com.javaweb.service.CompraAcaoService;
 import br.com.javaweb.transacoes.model.Compra;
+import br.com.javaweb.utils.MessagesAndRedirect;
+import br.com.javaweb.utils.Session;
 
 @ManagedBean
 @ViewScoped
@@ -16,11 +19,26 @@ public class ConsultaLucroCorretoraController {
 	private Corretora corretora;
 	private Compra compra;
 	private CompraAcaoService compraAcaoService;
+	private Investidor investidor;
 	
 	@PostConstruct
 	public void init(){
+		investidor = new Investidor();
+		investidor = buscarInvestidorSessao();
+		validarPermissao();
 		corretora = new Corretora();
 		corretora.setCompras(buscarTodasCompras());
+		
+	}
+	
+	private void validarPermissao(){
+		if(!(investidor.getLogin().equalsIgnoreCase("admin") || investidor.getLogin().equalsIgnoreCase(("adminisrtrador")))){
+			MessagesAndRedirect.redirecionarPara("fail.xhtml");
+		}
+	}
+	
+	private Investidor buscarInvestidorSessao(){
+		return (Investidor) Session.pegarSessao();
 	}
 			
 	private List<Compra> buscarTodasCompras(){

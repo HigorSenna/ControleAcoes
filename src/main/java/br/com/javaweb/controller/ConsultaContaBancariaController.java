86f92240172fbac0b708cac89b469337ef391c95
Higor Senna <higorr_jf@hotmail.com>
2016-07-02 18:40:12 -0300

    Criado Controller e Service Conta Bancaria, modificado persistence

---
+++ src/main/java/br/com/javaweb/controller/ConsultaContaBancariaController.java
@@ -0,0 +1,10 @@
+package br.com.javaweb.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class ConsultaContaBancariaController {
+
+}
---
+++ src/main/java/br/com/javaweb/service/ContaBancariaService.java
@@ -0,0 +1,5 @@
+package br.com.javaweb.service;
+
+public class ContaBancariaService {
+
+}
